8c56c89c1286722f2ccae24e49dcb32e9d91ff3d
manarelsayed95 <manarelsayed969@gmail.com>
2020-04-26 02:29:38 +0200

    part2

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
   #///////////////////////////////////////////////////////////////////////////////////////////////
 
   #welcome controllers
@@ -16,6 +15,11 @@ Rails.application.routes.draw do
   resources :articles do
   resources :comments
   end
+
+  #signup form 
+  # match ':controller(/:action(/:id))(.:format)'
+  resources :users
+  resources :sessions
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
