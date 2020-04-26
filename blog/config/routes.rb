df9b4e0994aa4c5e09a18881a9a4a65496f650da
manarelsayed95 <manarelsayed969@gmail.com>
2020-04-26 14:11:07 +0200

    delete users sessions

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'welcome/index'
   #///////////////////////////////////////////////////////////////////////////////////////////////
 
   #welcome controllers
@@ -15,11 +16,6 @@ Rails.application.routes.draw do
   resources :articles do
   resources :comments
   end
-
-  #signup form 
-  # match ':controller(/:action(/:id))(.:format)'
-  resources :users
-  resources :sessions
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
